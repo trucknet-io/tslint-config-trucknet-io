734cbe33e40ad4db4b6d92addfbccd87b4ca8467
Alexander Gusev <goooseman@me.com>
2019-08-02 10:51:49 +0300

    fix(tslint-react): jsx booleans omit

---
+++ test.tsx
@@ -0,0 +1,11 @@
+class Class {
+    public render () {
+        return <p className>D!</p>;
+    }
+}
+
+// class Class {
+//     public render () {
+//         return <p function={() => {}}>Do it!</p>;
+//     }
+// }
